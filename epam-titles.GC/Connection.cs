a3951d8bc3896f939f747e5fe066a1026790a9d7
Sergey V. <letsdrumm@gmail.com>
2020-01-27 16:32:45 +0300

    Добавлен пример работы сборщика мусора: финализатор и IDisposable

---
+++ epam-titles.GC/Connection.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace epam_titles.GC
+{
+    class Connection : IDisposable
+    {
+        public void Dispose()
+        {
+            System.GC.Collect();
+        }
+    }
+}
---
+++ epam-titles.GC/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace epam_titles.GC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var connection = new Connection())
+            {
+                Console.WriteLine("До создания объекта " + Convert.ToInt32(System.GC.GetTotalMemory(false)));
+                var obj = new Entity();
+                Console.WriteLine("После создания объекта " + Convert.ToInt32(System.GC.GetTotalMemory(false)));
+                Console.WriteLine("До сборки мусора (ссылка есть) " + System.GC.GetGeneration(obj)); 
+                System.GC.Collect();
+                Console.WriteLine("После сборки мусора (ссылка есть) " + System.GC.GetGeneration(obj));
+                obj = null;
+                Console.WriteLine("Пока открыт поток " + Convert.ToInt32(System.GC.GetTotalMemory(false)));
+            }
+
+            Console.WriteLine("После работы финализатора " + Convert.ToInt32(System.GC.GetTotalMemory(false)));
+        }
+    }
+}
