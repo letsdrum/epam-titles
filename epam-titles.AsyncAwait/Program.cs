43372c727f8f0fad9685eeb589b006b7ea707f62
Sergey V. <letsdrumm@gmail.com>
2020-01-27 15:51:33 +0300

    Добавлен пример использования async/await

--- epam-titles.AsyncAwait/Program.cs
+++ epam-titles.AsyncAwait/Program.cs
@@ -10,6 +10,23 @@ namespace epam_titles.AsyncAwait
     {
         static void Main(string[] args)
         {
+            NumbersForEachAsync("1");
+            NumbersForEachAsync("2");
+            Console.ReadLine();
+        }
+
+        static void NumbersForEach(string ThreadName)
+        {
+            for(int i = 0; i < int.MaxValue; i++)
+                if (i % 100000 == 0)
+                    Console.WriteLine($"Поток {ThreadName}");
+        }
+
+        static async void NumbersForEachAsync(string ThreadName)
+        {
+            Console.WriteLine($"Начало потока {ThreadName}");
+            await Task.Run(() => NumbersForEach(ThreadName));
+            Console.WriteLine($"Конец потока {ThreadName}");
         }
     }
 }
