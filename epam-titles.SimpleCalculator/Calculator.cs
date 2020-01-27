67914e649790c75a8e9e692f0dddd6ea4dec9546
Sergey V. <letsdrumm@gmail.com>
2020-01-27 16:55:13 +0300

    Добавлен проект Calculator, чтобы продемонстрировать работу Unit-тестов

---
+++ epam-titles.SimpleCalculator/Calculator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace epam_titles.SimpleCalculator
+{
+    public class Calculator
+    {
+        public static double Addition(double x, double y)
+        {
+            return x + y;
+        }
+        public static double Substract(double x, double y)
+        {
+            return x - y;
+        }
+        public static double Multiply(double x, double y)
+        {
+            return x * y;
+        }
+        public static double Divide(double x, double y)
+        {
+            return x / y;
+        }
+    }
+}
---
+++ epam-titles.SimpleCalculator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace epam_titles.SimpleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Calculator.Addition(10, 5));
+            Console.WriteLine(Calculator.Substract(10, 5));
+            Console.WriteLine(Calculator.Multiply(10, 5));
+            Console.WriteLine(Calculator.Divide(10, 3));
+
+            Console.ReadKey();
+        }
+    }
+}
